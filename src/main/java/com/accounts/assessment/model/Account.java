fe00c4992994e5e0b18a14674f1ae0d2a7b9e2f9
kvasu07 <93069797+kvasu07@users.noreply.github.com>
2021-10-25 03:30:23 +1100

    Update Account.java

--- src/main/java/com/accounts/assessment/model/Account.java
+++ src/main/java/com/accounts/assessment/model/Account.java
@@ -1,6 +1,8 @@
 package com.accounts.assessment.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -8,27 +10,32 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "accounts")
-public class Account {
+public class Account implements Serializable {
 
     @Id
+    @Column(name = "accountNumber")
     private Long accountNumber;
 
-    @Column
+    @Column(name = "accountName")
     private String accountName;
 
-    @Column
+    @Column(name = "accountType")
     private String accountType;
 
-    @Column
+    @Column(name = "currency")
     private String currency;
 
-    @Column
+    @Column(name = "balanceDate")
     private String balanceDate;
 
-    @Column
+    @Column(name = "openAvailableBalance")
     private BigDecimal openAvailableBalance;
+
 }
