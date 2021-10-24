c3f96ac5fd127edb4e811fef1ca50e545b05a2c7
kvasu07 <93069797+kvasu07@users.noreply.github.com>
2021-10-25 10:40:17 +1100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,8 @@ INSERT INTO accounts (accountNumber, accountName, accountType, currency, balance
 INSERT INTO accounts (accountNumber, accountName, accountType, currency, balanceDate, openAvailableBalance) VALUES
   (791066619, 'AUSavings933', 'Savings', 'AUD', '08/11/2018',88005.93);
 
-INSERT INTO transactions (id, accountNumber, accountName, currency, valueDate, debitAmount, creditAmount, debitCredit, transactionNarrative) VALUES
-  (1,585309209, 'Savings Account', 'SGD', 'Jan 12,2012',null ,9540.98, 'Credit','Salary Credit');
-INSERT INTO transactions (id, accountNumber, accountName, currency, valueDate, debitAmount, creditAmount, debitCredit, transactionNarrative) VALUES
-  (2,585309209, 'Savings Account', 'SGD', 'Jan 12,2012',603.45, null, 'Debit','Rent Deduction');
+INSERT INTO transactions (accountNumber, accountName, currency, valueDate, debitAmount, creditAmount, debitCredit, transactionNarrative) VALUES
+  (585309209, 'Savings Account', 'SGD', 'Jan 12,2012',null ,9540.98, 'Credit','Salary Credit');
+INSERT INTO transactions (accountNumber, accountName, currency, valueDate, debitAmount, creditAmount, debitCredit, transactionNarrative) VALUES
+  (585309209, 'Savings Account', 'SGD', 'Jan 12,2012',603.45, null, 'Debit','Rent Deduction');
 
