8ece3675ae53c7ab642d7438faa6511940f14c8c
kvasu07 <93069797+kvasu07@users.noreply.github.com>
2021-10-25 12:21:58 +1100

    Update AssessmentApplication.java

--- src/main/java/com/accounts/assessment/AssessmentApplication.java
+++ src/main/java/com/accounts/assessment/AssessmentApplication.java
@@ -2,14 +2,22 @@ package com.accounts.assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AssessmentApplication {
     public static void main(String[] args){
         SpringApplication.run(AssessmentApplication.class,args);
     }
+
+    @Bean
+    public Docket accountsApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.accounts.assessment")).build();
+    }
 }
-
-
-
-
